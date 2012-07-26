a7f1f875db48d7997575416da4fb9b9c2bef4c29
xuhaixiang <xuhaixiang@gmail.com>
2012-07-26 15:21:22 +0800

    add tidy for python

--- hrms/urls.py
+++ hrms/urls.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
@@ -7,20 +9,20 @@ from django.contrib.auth.views import login, logout
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
+urlpatterns = patterns(  # Examples:
+                         # url(r'^hrms/', include('hrms.foo.urls')),
+                         # Uncomment the admin/doc line below to enable admin documentation:
+                         # Uncomment the next line to enable the admin:
+    '',
     url(r'^$', 'ot.views.index', name='idx'),
-    # url(r'^hrms/', include('hrms.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ot/', include('ot.urls')),
-    url(r'^accounts/login/$', login, {'template_name':'ot/login.html'}),
+    url(r'^accounts/login/$', login, {'template_name': 'ot/login.html'
+        }),
     url(r'^accounts/logout/$', logout),
-)
+    )
 
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)

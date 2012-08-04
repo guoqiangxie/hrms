f19c91e8564f24a70375d6f2a2b84dfaa17e62c4
john.xie.pc <guoqiang.xie@jinjiang.com>
2012-08-04 22:27:49 +0800

    init models

--- hrms/ot/models.py
+++ hrms/ot/models.py
@@ -9,10 +9,49 @@ class Application(models.Model):
 
     def __unicode__(self):
         return self.subject
-    
+
+
 class Person(models.Model):
     app = models.ForeignKey(Application)
     name = models.CharField(max_length=10)
 
     def __unicode__(self):
         return self.name
+
+class Employee(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=40)
+    telephone = models.CharField(max_length=20)
+    level = models.CharField(max_length=10)
+
+    def __unicode__(self):
+            return self.name
+
+class Overtimeform(models.Model):
+    begintime = models.DateTimeField()
+    endtime = models.DateTimeField()
+    reason = models.CharField(max_length=255)
+    comment = models.CharField(max_length=255)
+    applyer = models.ForeignKey(Employee)
+    status = models.CharField(max_length=5)
+
+    def __unicode__(self):
+        return self.applyer + self.reason
+
+class Employee_overtimeform_ref(models.Model):
+    employee = models.ForeignKey(Employee)
+    overtimeform = models.ForeignKey(Overtimeform)
+
+    def __unicode__(self):
+        return self.employee + self.overtimeform
+
+
+class apply_track(models.Model):
+    overtimeform = models.ForeignKey(Employee_overtimeform_ref)
+    approval = models.ForeignKey(Employee)
+    approval_note = models.CharField(max_length=255)
+    apply_date = models.DateTimeField()
+    type = models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return self.approval + self.overtimeform + self.type

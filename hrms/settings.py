0fe781c23ff09302d9fc031d094e85133855a996
john.xie.pc <guoqiang.xie@jinjiang.com>
2012-08-04 22:30:07 +0800

    set database username and pwd to root

--- hrms/settings.py
+++ hrms/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'hrms',                      # Or path to database file if using sqlite3.
-        'USER': 'sean',                      # Not used with sqlite3.
-        'PASSWORD': 'sean',                  # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'root',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }

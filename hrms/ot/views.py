25a10abb925b0c17e8f7ee6056cf79c3333fc4f6
xuhaixiang <xuhaixiang@gmail.com>
2012-09-11 15:05:53 +0800

    refactory index method

--- hrms/ot/views.py
+++ hrms/ot/views.py
@@ -21,16 +21,6 @@ class FOvertime(forms.ModelForm):
                    }
 
 
-class FOvertimeRef(forms.ModelForm):
-    class Meta:
-        model = OvertimeRef
-
-
-@login_required
-def index(request):
-    return render(request, 'index.html')
-
-
 def page_logout(request):
     logout(request)
     return HttpResponseRedirect('/')
@@ -39,20 +29,27 @@ def page_logout(request):
 @login_required
 def new(request):
     ctx = {}
-    overtimeForm = FOvertime()
+    otf = FOvertime()
     ctx['model'] = request.user
     ctx['employees'] = User.objects.all()
-    ctx['form'] = overtimeForm
+    ctx['form'] = otf
+    ot = Overtime()
+    ot.apper = request.user
     if request.method == 'POST':
-        overtimeForm = FOvertime(request.POST)
-        if overtimeForm.is_valid():
-            overtimeForm = overtimeForm.save()
-            overtimeFormId = overtimeForm.id
-            oterIds = request.REQUEST.getlist('employee')
-            for oterId in oterIds:
-                employee_overtimeform_ref = OvertimeRef({'employee': oterId, 'overtimeform': overtimeFormId})
-                employee_overtimeform_ref.save()
+        otf = FOvertime(request.POST, instance=ot)
+        if otf.is_valid():
+            otf.save()
+            for oterId in request.REQUEST.getlist('employee'):
+                OvertimeRef(employee_id=oterId, overtimeform_id=ot.id).save()
             return HttpResponseRedirect(reverse('ot_idx'))
+        else:
+            print 'form is not valid', otf.errors
     return render(request, 'overtimeForm.html', ctx)
 
+
+@login_required
+def index(request):
+    ctx = {}
+    ctx['otList'] = Overtime.objects.all()
+    return render(request, 'index.html', ctx)
 
